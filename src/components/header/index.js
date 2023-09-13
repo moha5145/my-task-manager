cb8418b2375a0863c962c1030b16f163f0a2ea74
moha5145 <mms.jimma@gmail.com>
2023-09-13 15:45:54 +0200

    change app name

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,7 @@ import FlatButton from "../shared/buttons/FlatButton";
 const Header = () => {
   return (
     <div className="flex w-screen justify-between items-center py-1 sm:py-6 px-2 border-b-2">
-      <p>React ToDo</p>
+      <p className="text-xl font-bold text-gray-500">My task manager</p>
       <div className="flex gap-1">
         <FlatButton
           Icon={Lock}
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 const defaultData = [
   { title: "group 1", items: ["1", "2", "3"] },
